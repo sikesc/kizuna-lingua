ca23b4a2a77e5fef21d742a2151a3a836cf26d28
Lorenzo Panneman <unframed3@gmail.com>
2025-11-26 11:57:55 +0900

    Added topic explanation and grammar points to topic show page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,5 +5,9 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+    user_level = current_user.learning_level
+    user_language = current_user.learning_language
+    @grammar_points = @topic.grammar_points.where(level: user_level, language: user_language)
+    authorize @topic
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ApplicationRecord
+  has_many :topic_grammar_points
+  has_many :partnership_topics
   has_many :partnerships, through: :partnership_topics
   has_many :grammar_points, through: :topic_grammar_points
   has_many :challenges
