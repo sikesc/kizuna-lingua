135ee2d191e2ecd3076991e21874185930464db4
Colton Sikes <colton.sikes@gmail.com>
2025-11-26 11:51:22 +0900

    Started show page for challenges and started the system for updating journals

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,8 @@
 class ChallengesController < ApplicationController
+
+  def show
+    @challenge = Challenge.find(params[:id])
+    @journal = @challenge.journal
+    authorize @challenge
+  end
 end
