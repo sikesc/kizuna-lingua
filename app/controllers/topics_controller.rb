fdd9c2feeca397f7fcb85b0511ce65f75a7496d4
Lorenzo Panneman <unframed3@gmail.com>
2025-11-26 15:54:19 +0900

    working on topics index

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+    @topics = policy_scope(Topic)
+    @partnership = current_user.partnership
   end
 
   def show
