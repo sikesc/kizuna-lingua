27caa6cf222e4355149ed126ff69d2e878cff8b3
Colton Sikes <colton.sikes@gmail.com>
2025-11-27 18:10:25 +0900

    Corrected error and styled dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,12 +13,27 @@ class PagesController < ApplicationController
     @most_recent = PartnershipTopic.where(status: "in progress").order(created_at: :desc)
     if @most_recent.present?
       @most_recent_topic = @most_recent.first.topic
-      @partner_journal = @most_recent_topic.challenges.first.partner_journal(current_user)
-      @my_journal = @most_recent_topic.challenges.first.journal
+      @challenge = @most_recent_topic.challenges.first
+      @my_journal = Journal.find_by(user: current_user, challenge: @challenge, partnership: @partnership)
+      @partner_journal = @challenge.partner_journal(current_user)
+      @progress_percentage = calculate_progress_percentage(@my_journal, @partner_journal)
     end
     # if @most_recent_topic
     #   @recent_topic = @most_recent_topic.topic
     # end
+  end
+
+  private
+
+  def calculate_progress_percentage(my_journal, partner_journal)
+    progress = 0
+    max_stages = 5
+    progress += 1 if my_journal.present?
+    progress += 1 if partner_journal.present?
+    progress += 1 if my_journal.feedback.present?
+    progress += 1 if partner_journal.feedback.present?
+    progress += 1 if my_journal.conversation_status.present?
+    (progress.to_f / max_stages) * 100
   end
 
 end
