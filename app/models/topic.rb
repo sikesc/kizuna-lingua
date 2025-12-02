1e0f4c1a28d73148a9d15de0667d67bebfb4b4fc
Colton Sikes <colton.sikes@gmail.com>
2025-12-02 12:55:31 +0900

    added image generation functionality for creating topics"

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,7 @@ class Topic < ApplicationRecord
   has_many :partnerships, through: :partnership_topics
   has_many :grammar_points, through: :topic_grammar_points
   has_many :challenges
+  has_one_attached :image
 
   validates :name, presence: true, uniqueness: true
   validates :content, presence: true
